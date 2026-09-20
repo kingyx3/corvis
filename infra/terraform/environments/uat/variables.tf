e0fde1b0610e764400577157521d46aa30f40215
kingyx3 <kingyx3@hotmail.com>
2026-09-20 23:43:26 +0800

    infra: expose guarded decommission switch in uat

--- infra/terraform/environments/uat/variables.tf
+++ infra/terraform/environments/uat/variables.tf
@@ -36,3 +36,9 @@ variable "monthly_budget_amount_usd" {
   type    = number
   default = 500
 }
+
+variable "decommission_mode" {
+  description = "Set only by the guarded environment decommission workflow."
+  type        = bool
+  default     = false
+}
