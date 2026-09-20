6902cd6c6367874d7ddd6f3a012d6e5be25630b4
kingyx3 <kingyx3@hotmail.com>
2026-09-20 23:43:13 +0800

    infra: expose guarded decommission switch in dev

--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -1,2 +1,8 @@
 variable "project_id" { type = string }
 variable "source_bucket_name" { type = string }
+
+variable "decommission_mode" {
+  description = "Set only by the guarded environment decommission workflow."
+  type        = bool
+  default     = false
+}
