86a19d4fb40d23834a947a51778a5ea822124b09
kingyx3 <kingyx3@hotmail.com>
2026-09-21 23:16:27 +0800

    Expose customer web gateway outputs

---
+++ infra/terraform/modules/gcp-web-gateway/outputs.tf
@@ -0,0 +1,2 @@
+output "gateway_hostname" { value = google_api_gateway_gateway.web.default_hostname }
+output "edge_api_key" { value = google_apikeys_key.cloudflare_edge.key_string sensitive = true }
