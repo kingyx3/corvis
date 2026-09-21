27374b43a331d7896a2e242d47ed7fc8e4d695bf
kingyx3 <kingyx3@hotmail.com>
2026-09-21 15:37:38 +0800

    infra: expose worker runtime outputs

--- infra/terraform/modules/cloud-run-runtime/outputs.tf
+++ infra/terraform/modules/cloud-run-runtime/outputs.tf
@@ -7,10 +7,19 @@ output "api_service_uri" {
   sensitive = true
 }
 
-output "postgres_dsn_secret_id" {
-  value = google_secret_manager_secret.postgres_dsn.secret_id
+output "worker_service_name" {
+  value = try(google_cloud_run_v2_service.worker[0].name, null)
 }
 
-output "gateway_identity_secret_id" {
-  value = google_secret_manager_secret.gateway_identity.secret_id
+output "worker_service_uri" {
+  value     = try(google_cloud_run_v2_service.worker[0].uri, null)
+  sensitive = true
+}
+
+output "worker_audience" {
+  value = local.worker_audience
+}
+
+output "postgres_dsn_secret_id" {
+  value = google_secret_manager_secret.postgres_dsn.secret_id
 }
